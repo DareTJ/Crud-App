5a94e78f2ac2909db669cfe2bb30fb347f6e5145
Dare Tijani <oatijani1@gmail.com>
2020-07-15 21:42:57 +0100

    updated urls

--- employee_project/employee_register/views.py
+++ employee_project/employee_register/views.py
@@ -3,7 +3,7 @@ from .forms import EmployeeForm
 from .models import Employee
 
 def employee_list(request):
-    context = {'employee_list':Employee.objects.all()}
+    context = {'employee_list': Employee.objects.all()}
     return render(request, "employee_register/employee_list.html",context)
 
 def employee_form(request,id=0):
@@ -23,6 +23,7 @@ def employee_form(request,id=0):
         if form.is_valid:
             form.save()
         return redirect('/employee/list')
+
 def employee_delete(request,id):
     employee = Employee.objects.get(pk=id)
     employee.delete()
