afe66889dc902fe70a5081818b0054c7fbb819db
Dare Tijani <oatijani1@gmail.com>
2020-07-16 06:47:51 +0100

    Updated Settings

--- employee_project/settings.py
+++ employee_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 #DEBUG = True
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['myawesomecrudeapp.herokuapp.com']
+ALLOWED_HOSTS = ['mycrudeapp.herokuapp.com']
 
 
 # Application definition
